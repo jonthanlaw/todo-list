8f443c89971712127e3a954f298a903d1af5be9d
Jonathan Law <jonathanlawdev@gmail.com>
2020-02-22 17:15:27 -0800

    clicking submit outputs boxes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
-
 const formSubmitted = (e) => {
   event.preventDefault();
   const itemToAdd = document.querySelector('#todo__additem').value;
-  todoContainer.innerHTML += leftToDoDiv;
+
+  let toDoDiv = '';
+  toDoDiv += leftToDoDiv;
+  toDoDiv += 'test';
+  toDoDiv += rightToDoDiv;
+
+  console.log(toDoDiv);
+
+  todoContainer.innerHTML += toDoDiv;
+
 }
 
 const form = document.querySelector('.todo__form');
 const todoContainer = document.querySelector('.todo__itemcontainer');
-const leftToDoDiv = '<div class = '+ '"col col-sm-6 col-md-4 col-lg-3 justify-content-center">testtext</div>';
-
+const leftToDoDiv = '<div class = ' + '"col-12 col-sm-6 col-md-4 col-lg-3 toDoDiv">';
+const rightToDoDiv = '</div>';
 form.addEventListener('submit', formSubmitted);
