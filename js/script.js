f5aa31a6c7a64ca9b9c29f1775e26f679ad18fbc
Jonathan Law <jonathanlawdev@gmail.com>
2020-02-22 20:55:33 -0800

    fix bug with multiple updates, but now can only update last item

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // form submitted handler
-const formSubmitted = (e) => {
-  event.preventDefault();
+const formSubmitted = e => {
+  e.preventDefault();
   const itemToAdd = document.querySelector('#todo__additem').value;
 
   // If length 3 or greater, append item
@@ -12,7 +12,7 @@ const formSubmitted = (e) => {
 
     //Containing Div for task and task editor
     toDoDiv += '<div class ="row">'
-    toDoDiv += '<span class="">' + itemToAdd + '</span>';
+    toDoDiv += '<span class="todo__item" >' + itemToAdd + '</span>';
     toDoDiv += '<span class="todo__editor">' +
       '<i class="fa fa-pencil"></i>' +
       '</span>';
@@ -27,7 +27,8 @@ const formSubmitted = (e) => {
 
     // Add event listeners for the new buttons
     let editor = document.querySelectorAll('.todo__editor');
-    editor.forEach(element => element.addEventListener('click', editToDo));
+    editor[editor.length - 1].addEventListener('click', editToDo);
+    //  editor[editor.length]addEventListener('click', editToDo));
   }
   // If length less then 3, give error
   else {
@@ -40,23 +41,69 @@ const formSubmitted = (e) => {
 }
 
 
-const editToDo = (e) => {
+//  Event handler when pencil clicked - to edit to do item
+const editToDo = e => {
 
-  console.log(e.target)
+  // Container for to do item
+  let itemContainer = e.target.parentNode.parentNode;
+  //add update form for to do item
+  itemContainer.innerHTML += update;
+
+  //add event listener
+  const updateForm = itemContainer.children[2];
+  console.log(updateForm);
+  updateForm.addEventListener('submit', formUpdated);
+
+}
+
+
+// Event handler when updating to do item text
+const formUpdated = e => {
+  e.preventDefault();
+
+  //Container for to do item
+  let parentContainer = e.target.parentNode;
+
+  // todoText = text area for item
+  let todoText = parentContainer.firstElementChild;
+
+  //updateItem = what is in the input box to be used for update
+  updateItem = e.target.firstElementChild.value;
+
+  // set todoText = updatedItem
+  todoText.textContent = updateItem;
+
+  //Add back event listener for the button
+  parentContainer.children[1].addEventListener('click', editToDo);
+
+  // Remove Form
+  e.target.remove();
+  // Add Event Listeners so  edit button can be pushed again
+  //let currentEditor =
 }
 
 
 
 
 
-
-
-
-
+// form set to add to do button
 const form = document.querySelector('.todo__form');
 const todoContainer = document.querySelector('.todo__itemcontainer');
+
+//Surrounding container for each to do item
 const leftToDoDiv = '<div class = ' + '"col-12 col-sm-6 col-md-4 col-lg-3 toDoDiv">';
 const rightToDoDiv = '</div>';
+
+
+
+// Form for updating todo item when pencil clicked
+const update = '<form class="updateForm">' +
+  '<input class="" type="text"class="todo__updateText">' +
+  '<input class="" type="submit" class="todo__updateSubmit" value="Update">' +
+  '</form>';
+
+
+
 
 
 // Event listeners
