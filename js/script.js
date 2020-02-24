eded9aeb7935ed7bdeb9b23e2a0a05fb1adf4272
Jonathan Law <jonathanlawdev@gmail.com>
2020-02-23 18:39:44 -0800

    form + toggle complete working

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // form submitted handler
 const formSubmitted = e => {
   e.preventDefault();
-  const itemToAdd = document.querySelector('#todo__additem').value;
+  let itemToAdd = document.querySelector('#todo__additem').value;
 
   // If length 3 or greater, append item
   if (itemToAdd.length >= 3) {
@@ -12,15 +12,24 @@ const formSubmitted = e => {
 
     //Containing Div for task and task editor
     toDoDiv += '<div class ="row">'
-    toDoDiv += '<div>';
+    toDoDiv += '<div class = "col-9">';
     toDoDiv += '<span class="todo__item" >' + itemToAdd + '</span>';
     toDoDiv += '<span class="todo__editor">' +
       '<i class="fa fa-pencil"></i>' +
       '</span>';
     toDoDiv += '</div>';
-    // Closing Div
+
+    // Add complete icon
+    toDoDiv += complete;
+
+    //add delete icon
+    toDoDiv += remove;
+
+    // Closing Div for row
     toDoDiv += rightToDoDiv;
 
+    //Add form container
+    toDoDiv += '<div class="updateForm"></div>'
     //Main Container closing div
     toDoDiv += rightToDoDiv;
 
@@ -33,21 +42,36 @@ const formSubmitted = e => {
     // If the item doesn't have a current update box, add event listener to edit button
     for (todo of indiv_todoContainers) {
 
-      if (!todo.outerHTML.includes("updateForm")) {
+      if (!todo.outerHTML.includes("todo__updateText")) {
 
-
-        let editButton = todo.firstElementChild.firstElementChild.children[1];
+        let editButton = todo.firstElementChild.firstElementChild.children[1].children[0];
         editButton.addEventListener('click', editToDo);
-      }
-
-      //fix bug where click event disapears
-      else {
+      } else {
+        console.log('in here');
         //Add event listner for update button
+        console.log(todo.children[1].children[1]);
         todo.children[1].children[1].addEventListener('click', formUpdated);
       }
+
+
+    }
+    // Add Event listeners for complete button
+
+    let finishBtn = document.querySelectorAll('.btn--complete');
+    for (finish of finishBtn) {
+      finish.addEventListener('click', completeHandler);
+    }
+    // Add Event listeners for uncomplete button
+
+    let uncompleteBtn = document.querySelectorAll('.btn--uncomplete');
+    for (uncomplete of uncompleteBtn) {
+      uncomplete.addEventListener('click', uncompleteHandler);
     }
 
   }
+  // End of container for if length greater then 3
+
+
   // If length less then 3, give error
   else {
     // let toDoDiv = '';
@@ -59,16 +83,18 @@ const formSubmitted = e => {
 }
 
 
+
+
 //  Event handler when pencil clicked - to edit to do item
 const editToDo = e => {
   //Remove event listner for this button
-  //  e.target.removeEventListener('click', editToDo);
+  e.target.removeEventListener('click', editToDo);
   // Container for to do item
 
   let itemContainer = e.target.parentNode.parentNode.parentNode.parentNode;
 
   //add update form for to do item
-  itemContainer.innerHTML += update;
+  itemContainer.children[1].innerHTML += update;
 
   //add event listener
 
@@ -102,18 +128,57 @@ const formUpdated = e => {
   // Set new text in todo
   todoContainer.firstElementChild.firstElementChild.firstElementChild.textContent = updateItem;
 
-  // //Add back event listener for the button
-  todoContainer.firstElementChild.firstElementChild.children[1].addEventListener('click', editToDo);
+  //Add back event listener for the edit button
+  todoContainer.firstElementChild.firstElementChild.children[1].children[0].addEventListener('click', editToDo);
+
+  // Fix bug where toggle stops working
+  let finishBtn = document.querySelectorAll('.btn--complete');
+  for (finish of finishBtn) {
+    finish.addEventListener('click', completeHandler);
+  }
+  // Add Event listeners for uncomplete button
+
+  let uncompleteBtn = document.querySelectorAll('.btn--uncomplete');
+  for (uncomplete of uncompleteBtn) {
+    uncomplete.addEventListener('click', uncompleteHandler);
+  }
 
   // // Remove Form
-  e.target.parentNode.remove();
+  e.target.parentNode.innerHTML = "";
   // // Add Event Listeners so  edit button can be pushed again
   // //let currentEditor =
 }
 
+// Handler for completed task
+const completeHandler = (e) => {
+  //Remove event listener
+  e.target.removeEventListener('click', completeHandler);
 
+  //change to toggle button
+  e.target.classList.remove('fa-check');
+  e.target.classList.remove('btn--complete');
+  e.target.classList.add('fa-repeat');
+  e.target.classList.add('btn--uncomplete');
 
+  //add event listner to toggle
+  e.target.addEventListener('click', uncompleteHandler);
+}
 
+// Handler for toggling to uncomplete task
+const uncompleteHandler = (e) => {
+
+  //remove event listener
+  e.target.removeEventListener('click', uncompleteHandler);
+
+  // Change to check button
+  e.target.classList.remove('fa-repeat');
+  e.target.classList.remove('btn--uncomplete');
+  e.target.classList.add('fa-check');
+  e.target.classList.add('btn--complete');
+
+  // Add event listener for check
+  e.target.addEventListener('click', completeHandler);
+}
 
 
 // form set to add to do button
@@ -125,12 +190,15 @@ const leftToDoDiv = '<div class = ' + '"col-12 col-sm-6 col-md-4 col-lg-3 toDoDi
 const rightToDoDiv = '</div>';
 
 
+// Add complete and remove button
+const complete = '<div class = "col-3"><span><i class="fa fa-check btn--complete"></i></span>';
+const remove = '<span><i class="fa fa-times"></i></span></div>';
+
 
 // Form for updating todo item when pencil clicked
-const update = '<div class="updateForm">' +
-  '<input class="" type="text" class="todo__updateText">' +
-  '<button  class="todo__updateSubmit">Update</button>' +
-  '</div>';
+const update = '<input class="todo__updateText" type="text">' +
+  '<button  class="todo__updateSubmit">Update</button>';
+
 
 
 
